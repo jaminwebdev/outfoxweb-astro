4c5e0c7760cb979eb962a15579098873f6a13f38
Jamin <jaminwebdev@gmail.com>
2023-09-06 13:45:54 -0700

    Benefits Card

---
+++ src/components/cards/BenefitCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import InteractiveLottie from "../InteractiveLottie.svelte";
+
+	export let lottiePath = '';
+	export let delay = 0;
+	export let timeLoop = 4000;
+	export let speed = 1;
+	export let initialPlay = false;
+</script>
+
+<div
+	class="bg-secondary-bg-light dark:bg-secondary-bg-dark p-[25px] rounded-lg grid gap-2">
+	<div class="max-w-[60px]">
+		<InteractiveLottie
+			path={lottiePath}
+			repeat
+			bind:delay
+			bind:timeLoop
+			bind:speed
+			bind:initialPlay />
+	</div>
+	<h3 class="text-xl">
+		<slot name="heading" />
+	</h3>
+	<p>
+		<slot name="body" />
+	</p>
+</div>
