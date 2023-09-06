d461f4320d99b3acbe352bda96e7e87965dd17e1
Jamin <jaminwebdev@gmail.com>
2023-09-06 13:32:12 -0700

    Testimonial components and accessories added

---
+++ src/utils/testimonials.ts
@@ -0,0 +1,18 @@
+export type Testimonial = {
+    imgPath: string;
+    name: string;
+    body: string;
+};
+
+export const testimonials: Record<string, Testimonial> = {
+	kyle: {
+		name: 'Kyle',
+		imgPath: '/images/Placeholder_Headshot.jpg',
+		body: 'Foxcraft Digial saved my life and the life of my children.'
+	},
+	stacy: {
+		name: 'Stacy',
+		imgPath: '/images/Placeholder_Headshot.jpg',
+		body: `We can’t believe it - Our conversions doubled, and our revenue did too.`
+	}
+};
