2759384e65d6735630b6d4b99dc63cbce038404b
Jamin <jaminwebdev@gmail.com>
2024-11-04 10:27:05 -0600

    web-design page

---
+++ src/components/sections/web-design/GeneralFAQs.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Accordion from '@components/Accordion.svelte'
+</script>
+
+<section id="FAQs" class="flex flex-col items-center mt-[125px]">
+  <h2 class="text-center mb-5">FAQs</h2>
+  <Accordion>
+    {#snippet question()}
+      <span>How long does design take?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>How many revisions do you allow?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Do you provide copywriting or content as well?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+</section>
---
+++ src/components/sections/web-design/ProcessFAQs.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import Accordion from '@components/Accordion.svelte'
+</script>
+
+<section id="FAQs" class="flex flex-col items-center mb-[125px]">
+  <h2 class="text-center mb-5">How do we do it?</h2>
+  <Accordion isOpen>
+    {#snippet question()}
+      <span>Why isn't my website ranking on Google?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Why isn't my website ranking on Google?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Why isn't my website ranking on Google?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Wireframing & revisions</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Design & Revisions</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Delivery</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+</section>
+
