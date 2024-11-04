331b38003e4a83dc0ade0cefa15cf2935d795438
Jamin <jaminwebdev@gmail.com>
2024-11-04 09:33:19 -0600

    web-audit page

---
+++ src/components/sections/website-audit/GeneralFAQs.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Accordion from '@components/Accordion.svelte'
+</script>
+
+<section id="generalFAQs" class="flex flex-col items-center mt-[125px]">
+  <h2 class="text-center mb-5">FAQs</h2>
+  <Accordion isOpen>
+    {#snippet question()}
+      <span>What does a website audit cover?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Do you also fix the issues you find?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Do you have an example report I can view?</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+</section>
---
+++ src/components/sections/website-audit/ProcessFAQs.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import Accordion from '@components/Accordion.svelte'
+</script>
+
+<section id="processFAQs" class="flex flex-col items-center mb-[125px]">
+  <h2 class="text-center mb-5">How do we do it?</h2>
+  <Accordion isOpen>
+    {#snippet question()}
+      <span>Website crawl</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>UX Analysis</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Content and SEO Analysis</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+  <Accordion>
+    {#snippet question()}
+      <span>Analytics Analysis</span>
+    {/snippet}
+    {#snippet answer()}
+      <span>It depends</span>
+    {/snippet}
+  </Accordion>
+</section>
+
