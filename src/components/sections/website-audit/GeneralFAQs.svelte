238737690524cf4f48ffc3211472cd48d878698b
Jamin <jaminwebdev@gmail.com>
2024-11-19 14:40:55 -0600

    website audit content polish

--- src/components/sections/website-audit/GeneralFAQs.svelte
+++ src/components/sections/website-audit/GeneralFAQs.svelte
@@ -9,7 +9,14 @@
       <span>What does a website audit cover?</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>For the most part, a website audit covers:</p>
+      <ul class="list-inside list-disc">
+        <li>A website crawl</li>
+        <li>Content and SEO analysis</li>
+        <li>UX analysis</li>
+        <li>An analysis of your site's analytics</li>
+      </ul>
+      <p class="mt-4">With these discoveries in hand, we can help you formulate a plan moving forward, and even implement the plan if you so choose.</p>
     {/snippet}
   </Accordion>
   <Accordion>
@@ -17,15 +24,20 @@
       <span>Do you also fix the issues you find?</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        It depends - if you don't have a team that already works on your website, and we're familiar with the technology used to build your site, absolutely we can.
+      </p>
+      <p class="mt-4">
+        Otherwise, we're happy to provide a full list of suggestions to help your team fix any an all issues we find.
+      </p>
     {/snippet}
   </Accordion>
-  <Accordion>
+  <!-- <Accordion>
     {#snippet question()}
       <span>Do you have an example report I can view?</span>
     {/snippet}
     {#snippet answer()}
       <span>It depends</span>
     {/snippet}
-  </Accordion>
+  </Accordion> -->
 </section>
--- src/components/sections/website-audit/ProcessFAQs.svelte
+++ src/components/sections/website-audit/ProcessFAQs.svelte
@@ -6,34 +6,50 @@
   <h2 class="text-center mb-5">How do we do it?</h2>
   <Accordion isOpen>
     {#snippet question()}
-      <span>Website crawl</span>
+      <span>1. Website crawl</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        First we crawl your entire website looking for broken links, images, and improperly structured HTML-related problems.
+        Nothing else matters as much as fixing broken links, identifying missing pages or images, and overall getting a survey of where your site stands.
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
     {#snippet question()}
-      <span>UX Analysis</span>
+      <span>2. UX Analysis</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        A UX (User Experience) analysis is a comprehensive evaluation of how users interact with a website or app. 
+        It examines the overall usability, navigation, design, and functionality to identify any pain points or barriers that might hinder a positive user experience. 
+      </p>
+      <p class="mt-4">
+        The goal is to uncover areas where users may get frustrated, confused, or leave the site, and recommend improvements to make the experience smoother, more intuitive, and engaging. By analyzing user behavior and feedback, a UX analysis helps create a more efficient and enjoyable experience, ultimately improving conversion rates and customer satisfaction.
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
     {#snippet question()}
-      <span>Content and SEO Analysis</span>
+      <span>3. Content and SEO Analysis</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        Leveraging the website crawl, we can identify pages with "thin content" (low word count), missing h1 tags, slow load times, and poorly written page tiles & meta descriptions.
+      </p>
+      <p class="mt-4">
+        With our expertise, we can formulate a plan to enhance your website's SEO content in the present and going forward.
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
     {#snippet question()}
-      <span>Analytics Analysis</span>
+      <span>4. Analytics Analysis</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        We also use your past and present analytics to help with figuring out where users are bouncing from your site, how they're using your navigation, and discover unforeseen wins lurking beneath.
+      </p>
     {/snippet}
   </Accordion>
 </section>
