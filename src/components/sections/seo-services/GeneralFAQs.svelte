2895df0effc78e3d35b2a2715c8cd6014315cfd2
Jamin <jaminwebdev@gmail.com>
2024-11-20 13:10:13 -0600

    SEO services polish

--- src/components/sections/seo-services/GeneralFAQs.svelte
+++ src/components/sections/seo-services/GeneralFAQs.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import Accordion from '@components/Accordion.svelte'
+  import MultiStepDialog from '@/components/dialogs/MultiStepDialog.svelte';
+import Accordion from '@components/Accordion.svelte'
 </script>
 
 <section id="FAQs" class="flex flex-col items-center mt-[125px]">
@@ -9,7 +10,16 @@
       <span>How long does SEO take to see results?</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        The time it takes to see results from SEO (Search Engine Optimization) can vary significantly depending on several factors. On average, it may take 3 to 6 months to start seeing noticeable improvements in rankings, traffic, and conversions. However, here are some key elements that influence the timeline:
+      </p>
+      <ul class="list-disc list-inside mt-2">
+        <li>Website age and authority</li>
+        <li>Competition</li>
+        <li>Keyword difficulty</li>
+        <li>Backlinks</li>
+        <li>and many more factors</li>
+      </ul>
     {/snippet}
   </Accordion>
   <Accordion>
@@ -17,7 +27,12 @@
       <span>How many words should a page or blog be?</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        There's no hard and fast rule on blog post length. In general, longer posts do tend to perform better, but the most important factor to keep in mind is quality. Well-written and well-planned posts will rank higher every time.
+      </p>
+      <p class="mt-4">
+        With that said, on average most posts should be between 750-1,500 words. Blog posts that are 1,500-3,000 are the longer posts that generally rank well, but only if the content is consitently valuable to users.
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
@@ -25,7 +40,12 @@
       <span>Why isn't my website ranking on Google?</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p class="mb-4">
+        There could be many, many factors. Let's figure it out together!
+      </p>
+      <MultiStepDialog>
+        Let's connect
+      </MultiStepDialog>
     {/snippet}
   </Accordion>
 </section>
--- src/components/sections/seo-services/ProcessFAQs.svelte
+++ src/components/sections/seo-services/ProcessFAQs.svelte
@@ -9,7 +9,10 @@
       <span>Discovery</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        In order to find out where you're going, we need to figure out where you've been. 
+        That's why our discovery starts with assessing your current rankings, keywords, UX, and design shortcomings.
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
@@ -17,17 +20,10 @@
       <span>Market and competitor analysis</span>
     {/snippet}
     {#snippet answer()}
-      <div>
-        <p>
-          Absolutely! Not only can we manage your hosting needs, but we prefer it. This allows us to
-          seamlessly update your website monthly, with 0 effort on your part.
-        </p>
-        <p class="mt-5">
-          On top of that, we don’t just launch your website and leave it up to chance. We monitor your
-          website by crawling it every 10 minutes to ensure your pages are always up and available. In
-          the event a page goes down, we’re notified in 10 minutes or less.
-        </p>
-      </div>
+      <p>
+        After exploring your website, we also assess your competition - whether local or online. 
+        What are they targeting? What is and isn't working for them? What's the market cap on your industry?
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
@@ -35,7 +31,13 @@
       <span>Keyword Research</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        Identifying and analyzing the words and phrases that people enter into search engines (like Google) when looking for information, products, or services related to your business or website.
+      </p>
+      <p class="mt-4">
+        The goal of keyword research is to understand the search intent behind these queries and select keywords that can help drive relevant traffic to your website, improve visibility in search engine results, and optimize content for better ranking.
+        It can also inform us on how to restructure your site's content if necessary.
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
@@ -43,7 +45,12 @@
       <span>Content Strategy</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        With the previous steps in mind, we move on to optimizing the text, images, videos, and other media—designed to achieve specific business goals. 
+      </p>
+      <p class="mt-4">
+        In doing so, we're creating valuable, relevant, and consistent content that resonates with a target audience, builds brand awareness, drives engagement, and increases conversions.
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
@@ -51,7 +58,10 @@
       <span>On page optimizations</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        It's not only the content you see on the screen, but also what search engines see that matters.
+        That's why we turn our focus on your title tags, meta descriptions, url structure, heading tags, schema markup, and linking opportunities.
+      </p>
     {/snippet}
   </Accordion>
   <Accordion>
@@ -59,7 +69,9 @@
       <span>Delivery</span>
     {/snippet}
     {#snippet answer()}
-      <span>It depends</span>
+      <p>
+        The final package can come in two ways - a report with all our findings and recommendations for your team to act on, or we can do the work ourselves. Your choice.
+      </p>
     {/snippet}
   </Accordion>
 </section>
