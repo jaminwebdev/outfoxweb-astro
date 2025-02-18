e645c57709651e9793e3083300383ce53885b2aa
Jamin <jaminwebdev@gmail.com>
2025-02-18 14:02:28 -0600

    upgrade astro and remove tailwind addon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 import mdx from "@astrojs/mdx";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx()],
+  integrations: [svelte(), mdx()],
   output: "static",
-  adapter: vercel()
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
