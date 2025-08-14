9f3511c3ba2a268bbe469417ff61c5e1ba29dd49
Jamin <jaminwebdev@gmail.com>
2025-08-13 22:35:51 -0500

    convex wrapper

---
+++ src/components/ConvexWrapper.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  type Props = {
+    children?: Snippet;
+  };
+
+  let { children }: Props = $props();
+
+  $effect(() => console.log('initializing Convex'));
+</script>
+
+{@render children?.()}
